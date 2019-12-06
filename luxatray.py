708f6d52bf2e0f2a2d06594d8548c1475a80dff6
Claes Johansson <claes.johansson@su.se>
2019-12-06 15:08:32 +0100

    Fixed to work with luxe.

--- luxatray.py
+++ luxatray.py
@@ -1,5 +1,6 @@
+#!/usr/bin/env python3
 import os
-import hid
+import datetime
 from gi.repository import Gtk as gtk, AppIndicator3 as appindicator
 
 
@@ -15,53 +16,50 @@ def menu():
     menu = gtk.Menu()
 
     cmd_off = gtk.MenuItem('Off')
-    cmd_off.connect('activate', lightOff)
+    cmd_off.connect("activate", off)
     menu.append(cmd_off)
-    
-    cmd_red = gtk.MenuItem('Red')
-    cmd_red.connect('activate', red)
+
+    cmd_red = gtk.MenuItem('Available')
+    cmd_red.connect("activate", available)
     menu.append(cmd_red)
-    
-    cmd_green = gtk.MenuItem('Green')
-    cmd_green.connect('activate', green)
+
+    cmd_green = gtk.MenuItem('Busy')
+    cmd_green.connect("activate", busy)
     menu.append(cmd_green)
-    
-    cmd_blue = gtk.MenuItem('Blue')
-    cmd_blue.connect('activate', blue)
+
+    cmd_blue = gtk.MenuItem('KA')
+    cmd_blue.connect("activate", ka)
     menu.append(cmd_blue)
-    
-    cmd_exit = gtk.MenuItem('Exit')
-    cmd_exit.connect('activate', quit)
-    menu.append(cmd_exit)
+
+    cmd_helg = gtk.MenuItem('Helg')
+    cmd_helg.connect("activate", helg)
+    menu.append(cmd_helg)
+
+    cmd_quit = gtk.MenuItem("Exit")
+    cmd_quit.connect("activate", quit)
+    menu.append(cmd_quit)
 
     menu.show_all()
     return menu
 
+def off(_):
+    os.system("luxe off")
 
-def lightOff(_):
-	off = [0, 0, 0]
-	writeToLux(off)
-    
-def red(_):
-	red = [255, 0, 0]
-	writeToLux(red)
-    
-def green(_):
-	green = [0, 128, 0]
-	writeToLux(green)
+def available(_):
+    os.system("luxe available")
 
-def blue(_):
-	blue = [0, 0, 255]
-	writeToLux(blue)
+def busy(_):
+    os.system("luxe busy")
+
+def ka(_):
+    os.system("luxe ka")
+
+def helg(_):
+    os.system("luxe helg")
+
 
 def quit(_):
     gtk.main_quit()
-    
-def writeToLux(color):
-	device = hid.device()
-	device.open(0x04D8, 0xF372)
-	device.write([2] + [0x41] + color + [10])
-	device.close()
 
 if __name__ == "__main__":
     main()
